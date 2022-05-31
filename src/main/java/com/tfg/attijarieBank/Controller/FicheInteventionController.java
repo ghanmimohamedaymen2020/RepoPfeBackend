b6bb61a1c1fa1b835570322ae7b8fecc30b90b8f
ghanmimohamedaymen2020 <ghanmimohamedaymen@gmail.com>
2022-05-31 13:41:34 +0100

    add package configure & class config crosconfiguration

--- src/main/java/com/tfg/attijarieBank/Controller/FicheInteventionController.java
+++ src/main/java/com/tfg/attijarieBank/Controller/FicheInteventionController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +21,6 @@ import com.tfg.attijarieBank.exception.ResourceNotFoundException;
 
 
 @RestController
-@CrossOrigin(origins = "*")
 
 
 public class FicheInteventionController {
